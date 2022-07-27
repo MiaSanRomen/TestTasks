4522dadca46199ac6975bdfb34481fd99594d7a0
Roman Brizinski <62285014+MiaSanRomen@users.noreply.github.com>
2022-07-27 22:57:35 +0300

    Added c# files

---
+++ Task 1. Pythagorean triple/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TestTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 0;                 //variable for counting iterations of solution
+            Console.WriteLine("First solution:");
+            for (int n = 1; n < 100; n++)
+            {
+                for (int m = n + 1; m < 100; m++)
+                {
+                    iterations++;
+                    if (GetAbcEuclid(m, n))
+                    {
+                        Console.WriteLine($"Number of iterations: {iterations}");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nSecond solution:");
+            for (int n = 1; n < 100; n++)
+            {
+                if (GetAbcDiscriminant(n))
+                {
+                    Console.WriteLine($"Number of iterations: {n}");
+                    break;
+                }
+            }
+        }
+
+        public static bool GetAbcEuclid(int m, int n)           //This method is used to verify if 'm' and 'n' can generate Pythagorean triple we need
+        {
+            int a = m * m - n * n;                              //Euclidean formulas for generating Pythagorean triples
+            int b = 2 * m * n;
+            int c = m * m + n * n;
+            int sum = a + b + c;
+            int product = a * b * c;
+
+            if (sum == 1000)                                   //Validating are these 'a', 'b', 'c' what we need.
+            {
+                Console.WriteLine($"m={m}, n={n}");
+                Console.WriteLine($"a={a}, b={b}, c={c}");
+                Console.WriteLine($"Summ: {sum}");
+                Console.WriteLine($"Product abc: {product}");
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool GetAbcDiscriminant(int n)            //This method is used to verify if 'n' can be used for creating discriminant to find 'm' we need
+        {                                                       //Better solution. Details in attached file
+            double discriminant = n * n + 2000;
+            if (Math.Sqrt(discriminant) % 1 == 0)               //Validate if discriminant is integer
+            {
+                int m = ((int) Math.Sqrt(discriminant) - n) / 2;         //Getting 'm' with help of discriminant 
+                return GetAbcEuclid(m, n);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task 2. Collatz Problem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<uint, uint> lengthFromEachNumber = new Dictionary<uint, uint> {{1, 1}};  //This dictionary will storage start numbers and 
+            uint maximumChainLength = 0;                                                        //length of their chains. I use dictionary because
+            uint maximumChainStartNumber = 0;                                                   //search complexity in it is O(1)
+            for (uint startNumber = 2; startNumber <= 1000000; startNumber++)
+            {
+                uint chainNumber = startNumber;
+                uint currentChainLength = 0;
+                while (true)                            //As all chains lead to 1, we can use while(true)
+                {
+                    if (lengthFromEachNumber.ContainsKey(chainNumber))                 //If dictionary keys contains generated in chain number,  
+                    {                                                                  //there is no need to generate the chin one more time
+                        currentChainLength += lengthFromEachNumber[chainNumber];       //So we just add to this chain's length one already founded before
+                        lengthFromEachNumber.Add(startNumber, currentChainLength);     //Add the start number and the length of his chain 
+                        break;
+                    }
+
+                    if (chainNumber % 2 == 1)                             //Here program generate next number if current was not founded in dictionary
+                    {
+                        chainNumber = 3 * chainNumber + 1;
+                    }
+                    else
+                    {
+                        chainNumber = chainNumber / 2;
+                    }
+
+                    currentChainLength++;
+                }
+
+                if (maximumChainLength < currentChainLength)                //If length of current chain is greater, save it instead of previous maximum
+                {
+                    maximumChainLength = currentChainLength;
+                    maximumChainStartNumber = startNumber;
+                }
+            }
+            Console.WriteLine($"\nMaximum Chain = {maximumChainLength}");              
+            Console.WriteLine($"Start Number = {maximumChainStartNumber}");
+        }
+    }
+}
